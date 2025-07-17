4aef1d1144dcff11f2b4ce1814ea467d8089ed1a
YiboZhao <51275903033@stu.ecnu.edu.cn>
2025-07-17 14:47:00 +0000

    initialized prompt, only instructions in the sys prompt, no few shots.

--- prompts.py
+++ prompts.py
@@ -1,3 +1,29 @@
 Prompt_templates = {
-    ""
+    "default":[
+        {
+            "role": "system",
+            "content": """\
+You are an evidence‑validation assistant. For each query, you are given a question wrapped inside the <question>...</question> tag, and a series of documents as evidence wrapped inside the <ref>...</ref> tag. To solve these questions, you must follow this **exact** process without deviation:
+
+### Instructions:
+
+1. You need to first figure out if the evidences are relevant and useful to the question. 
+
+    - If the evidences are **NOT** relevant, you should recall your knowledge about this question and wrap the process of recalling inside the token of <llm>...</llm>. 
+
+    - If the evidences are relevant, you should select the evidence that is most relevant to the question and wrap the selected texts inside the token of <evidence>...</evidence>. 
+
+2. You should then answer the question based on the selected evidence or your knowledge. The answer should be wrapped inside the token of <answer>...</answer>."""
+        },
+        {
+            "role": "user",
+            "content": """\
+Here is the question and the references: 
+<question>{question}</question>
+<ref>
+    {ref}
+</ref>
+"""
+        }
+    ]
 }
