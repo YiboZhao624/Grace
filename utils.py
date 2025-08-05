66305cba6f03c3be89e846e26fd6b578d43760f8
YiboZhao <51275903033@stu.ecnu.edu.cn>
2025-08-05 06:23:10 +0000

    add the merge_by_reverse_removal function. for the data generator.

--- utils.py
+++ utils.py
@@ -16,3 +16,35 @@ def load_raw_qasper_data(data_folder: str, split = "train"):
 def save_parquet(data: List[Dict], path: str):
     df = pd.DataFrame(data)
     df.to_parquet(path)
+
+def merge_by_reverse_removal(a: list[int], b: list[int]) -> list[int]:
+    """
+    a : the retrieved evidence chunk ids.
+    b : the gt_ground truth chunk ids to merge into a.
+    return : the merged chunk ids, the length should be equal to max(len(a), len(b)).
+    """
+    set_a = set(a)
+    set_b = set(b)
+
+    to_add = [x for x in b if x not in set_a]  # O(len(b))
+    to_remove = set_a - set_b  # O(len(a))
+
+    num_to_remove = len(to_add)
+    result = []
+    removed = 0
+
+    for x in reversed(a):
+        if x in to_remove and removed < num_to_remove:
+            removed += 1
+        else:
+            result.append(x)
+
+    result.reverse()
+    result.extend(to_add)
+
+    return result
+
+if __name__ == "__main__":
+    b = [4, 17, 52, 6]
+    a = [3,4,17,73,5,8,10,6]
+    print(merge_by_reverse_removal(a, b))
