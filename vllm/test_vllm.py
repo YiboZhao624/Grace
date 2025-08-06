80bbae32ebc40ec143961b897f561c6066996fc4
YiboZhao <51275903033@stu.ecnu.edu.cn>
2025-08-06 08:17:00 +0000

    add the test code for each model.

--- vllm/test_vllm.py
+++ vllm/test_vllm.py
@@ -2,6 +2,10 @@ from warnings import resetwarnings
 import requests
 from typing import Union, List
 import numpy as np
+import urllib3
+
+# 禁用SSL警告
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 documents = [
     "Reranking is fun!",
@@ -22,7 +26,7 @@ def vllm_retrieve(base_url: str, model: str, documents: Union[list[str], str]) -
         "model": model,
         "input": documents,
     }
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(url, headers=headers, json=data, verify=False)
     return np.array([response.json()["data"][i]["embedding"] for i in range(len(documents))])
 
 def vllm_rerank(base_url: str, model: str, query: str, documents: List[str]):
@@ -34,7 +38,7 @@ def vllm_rerank(base_url: str, model: str, query: str, documents: List[str]):
         "documents": documents,
         "top_n": len(documents)
     }
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(url, headers=headers, json=data, verify=False)
     print(response.json())
     return zip([response.json()["results"][i]["document"] for i in range(len(documents))], [response.json()["results"][i]["relevance_score"] for i in range(len(documents))])
 
@@ -45,11 +49,25 @@ def vllm_llm(base_url: str, model: str, prompt: str):
         "model": model,
         "messages": [{"role": "user", "content": prompt}],
     }
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(url, headers=headers, json=data, verify=False)
     print(response.json())
     return response.json()["choices"][0]["message"]["content"]
 
 if __name__ == "__main__":
+    base_url = f"http://localhost:8001"
+    model  = "/root/shared_planing/LLM_model/Qwen3-Embedding-0.6B"
+    documents = [
+        "Reranking is fun!",
+        "The capital of France is Paris",
+        "vLLM is an open-source framework for fast AI serving",
+    ]
+    query = "What is the capital of France?"
+    res = vllm_retrieve(base_url, model, documents)
+    print(res)
+    base_url = f"http://localhost:8002"
+    model = "/root/shared_planing/LLM_model/BAAI-bge-reranker-v2-m3/"
+    res = vllm_rerank(base_url, model, query, documents)
+    print(res)
     base_url = f"http://localhost:8003" 
     model = "/root/shared_planing/LLM_model/Qwen2.5-7B-Instruct"
     prompt = "What is the capital of France?"
