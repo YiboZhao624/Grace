f4f53a1af6a6d41e1c20f74f56ff09ff965b528d
YiboZhao <51275903033@stu.ecnu.edu.cn>
2025-08-07 11:09:34 +0000

    add the inference model in the output file name to distinguish the different results.

--- inference.py
+++ inference.py
@@ -3,24 +3,32 @@
 
 from llm import vLLM
 from utils import read_parquet, save_parquet, setup_logging, extract_answer_or_all, extract_evidence_or_all
-from configs import InferenceConfigs
 from typing import List, Dict, Literal
 from evaluator import Evaluator
 import json
 from tqdm import tqdm
+from configs import LLMConfig, InferenceConfigs
 
 logger = setup_logging("Inference")
 
 custom_InferenceConfigs = InferenceConfigs(
+    llm_config = LLMConfig(
+        model_name = "/root/shared_planing/LLM_model/Qwen3-4B-Instruct-2507",
+        url = "http://localhost:8004"
+    ),
     data_path = ["data/processed/0806-256cs/QASPER_Split-test_Prompt-default_NumberTemplate-False_retriever_bm25_TopK-7_WO_GT_Evidence_Rate-0.2.parquet","data/processed/0806-256cs/QASPER_Split-test_Prompt-default_NumberTemplate-False_retriever_reranker_vllm-Qwen3-Embedding-0.6B_TopK-7_WO_GT_Evidence_Rate-0.2.parquet","data/processed/0806-256cs/QASPER_Split-test_Prompt-default_NumberTemplate-False_retriever_vllm-Qwen3-Embedding-0.6B_TopK-7_WO_GT_Evidence_Rate-0.2.parquet"]
 )
 custom_llm = vLLM(custom_InferenceConfigs.llm_config)
 
-def inference(data: List[Dict]):
-    for item in tqdm(data, desc="Inference"):
+def inference(data: List[Dict], resume: int = 0):
+    for i, item in enumerate(tqdm(data[resume:], desc="Inference")):
         sys_prompt = item["prompt"][0]
         user_input = item["prompt"][1]
         answer = custom_llm.generate(user_input, sys_prompt)
+        if answer == "ERROR: THE MODEL CANNOT PROCESS THE REQUEST.":
+            logger.error(f"Fail to process the input, the user input is {user_input}, the sys prompt is {sys_prompt}")
+            logger.error(f"please resume the inference process from number {resume + i}.")
+            break
         item["answer"] = answer
     return data
 
@@ -28,10 +36,13 @@ def inference(data: List[Dict]):
 if __name__ == "__main__":
     for data_path in custom_InferenceConfigs.data_path:
         data = read_parquet(data_path)
-        logger.info(f"inference {data_path} now.")
+
+        llm_name = custom_InferenceConfigs.llm_config.model_name.split("/")[-1]
+        saving_path = data_path.replace(".parquet", f"_{llm_name}_inference.parquet")
+        logger.info(f"inference {data_path} with {llm_name} now.")
         inference(data)
-        save_parquet(data, data_path.replace(".parquet", "_inference.parquet"))
-        logger.info(f"inference done, the data is saved to {data_path.replace('.parquet', '_inference.parquet')}")
+        save_parquet(data, saving_path)
+        logger.info(f"inference done, the data is saved to {saving_path}")
         logger.info(f"the first data is: {data[0]}")
         
         enabled_metrics = ["BS","EM","RL","BL","RR"]
