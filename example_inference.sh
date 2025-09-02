6e97669b479567c63d7f1e2a8cc652ac731788e0
YiboZhao <51275903033@stu.ecnu.edu.cn>
2025-09-02 02:54:38 +0000

    add the example inference script for easily handing on.

---
+++ example_inference.sh
@@ -0,0 +1,5 @@
+python inference.py\
+    --llm_name Qwen/Qwen3-8B \
+    --url http://localhost:8000 \
+    --path \
+    data/processed/QASPER-test_test-retriever_reranker-512cs-vllm_Qwen3-0.6B-vllm_-top3-wogt04.parquet \
