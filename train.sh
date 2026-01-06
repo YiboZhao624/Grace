0f695883cbd89ae04d9380f6c48aaa3381bed80e
Yibo Zhao <yibozhao@stu.ecnu.edu.cn>
2026-01-06 13:34:59 +0000

    update the train scripts.

--- train.sh
+++ train.sh
@@ -1,45 +1,47 @@
 #!/usr/bin/env bash
 export HYDRA_FULL_ERROR=1
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-CUDA_VISIBLE_DEVICES=4,5,6,7 ray start --head
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+CUDA_VISIBLE_DEVICES=0,1,2,3 ray start --head
+
+num_gpu=4
 
 set -xeuo pipefail
 
-project_name='DAPO-0825-balanced-reward'
-exp_name='DAPO-Qwen3-8B-QASPER-Merged-Deduplicated-0825-4xA800'
+project_name='YOUR PROJECT NAME'
+exp_name="YOUR EXP NAME"
 
 adv_estimator=grpo
 
 use_kl_in_reward=False
-kl_coef=0.0
 use_kl_loss=False
-kl_loss_coef=0.0
+kl_loss_coef=0.001
 
 clip_ratio_low=0.2
 clip_ratio_high=0.28
 
-max_prompt_length=$((1024 * 8))
-max_response_length=$((1024 * 2))
+max_prompt_length=$((1024 * 5))
+max_response_length=$((1024 * 3))
 enable_overlong_buffer=True
-overlong_buffer_len=$((1024))
+overlong_buffer_len=$((1024 * 1))
 overlong_penalty_factor=1.0
 
 loss_agg_mode="token-mean"
 
-train_prompt_bsz=16
-n_resp_per_prompt=8               
-train_prompt_mini_bsz=4         
+train_prompt_bsz=32
+n_resp_per_prompt=8
+train_prompt_mini_bsz=8
+train_prompt_micro_batch_size_per_gpu=2
 
 NNODES=${NNODES:-1}               
-NGPUS_PER_NODE=${NGPUS_PER_NODE:-4}   
+NGPUS_PER_NODE=${NGPUS_PER_NODE:-${num_gpu}}   
 
 # Paths
 RAY_DATA_HOME=${RAY_DATA_HOME:-"${HOME}"}
-MODEL_PATH=${MODEL_PATH:-"${RAY_DATA_HOME}/shared_planing/LLM_model/Qwen3-8B"}
-CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/projects/RLRAG/ckpts/${project_name}/${exp_name}"}
-TRAIN_FILE=${TRAIN_FILE:-"${RAY_DATA_HOME}/projects/RLRAG/data/merged/0822-QASPER.parquet"}
-VAL_FILE=${VAL_FILE:-"${RAY_DATA_HOME}/projects/RLRAG/data/processed/0806-256cs/QASPER_Split-val_Prompt-default_NumberTemplate-False_retriever_bm25_TopK-10_WO_GT_Evidence_Rate-0.2.parquet"}
-
+MODEL_PATH=${MODEL_PATH:-"${RAY_DATA_HOME}/MODEL/PATH"}
+CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/CKPT/PATH/${project_name}/${exp_name}"}
+TRAIN_FILE=${TRAIN_FILE:-"${RAY_DATA_HOME}/PROJ/PATH/data/SPECIFIC/DATAFILE"}
+VAL_FILE=${VAL_FILE:-"${RAY_DATA_HOME}/PROJ/PATH/data/SPECIFIC/DATAFILE"}
 
 # Algorithm
 temperature=1.0
@@ -54,9 +56,8 @@ actor_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 2))
 infer_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 3))
 offload=False                     
 gen_tp=1                          
-fsdp_size=4                       
+fsdp_size=${num_gpu}                       
 # remember to set VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 for this model
-# 添加调试信息
 echo "=== debug ==="
 echo "NNODES: ${NNODES}"
 echo "NGPUS_PER_NODE: ${NGPUS_PER_NODE}"
@@ -76,8 +77,10 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=${adv_estimator} \
     algorithm.use_kl_in_reward=${use_kl_in_reward} \
     algorithm.kl_ctrl.kl_coef=${kl_coef} \
+    algorithm.kl_penalty=${kl_penalty} \
     actor_rollout_ref.actor.use_kl_loss=${use_kl_loss} \
     actor_rollout_ref.actor.kl_loss_coef=${kl_loss_coef} \
+    actor_rollout_ref.actor.kl_loss_type=${kl_loss_type} \
     actor_rollout_ref.actor.clip_ratio_low=${clip_ratio_low} \
     actor_rollout_ref.actor.clip_ratio_high=${clip_ratio_high} \
     actor_rollout_ref.actor.clip_ratio_c=10.0 \
@@ -91,19 +94,19 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
     actor_rollout_ref.model.path="${MODEL_PATH}" \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.model.lora_rank=16 \
-    actor_rollout_ref.model.lora_alpha=32 \
-    actor_rollout_ref.actor.optim.lr=5e-6 \
-    actor_rollout_ref.actor.optim.lr_warmup_steps=20 \
+    actor_rollout_ref.actor.optim.warmup_style=cosine \
+    actor_rollout_ref.actor.optim.lr=2e-6 \
+    actor_rollout_ref.actor.optim.lr_warmup_steps=50 \
     actor_rollout_ref.actor.optim.weight_decay=0.1 \
     actor_rollout_ref.actor.ppo_mini_batch_size=${train_prompt_mini_bsz} \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=${train_prompt_micro_batch_size_per_gpu} \
     actor_rollout_ref.actor.fsdp_config.param_offload=${offload} \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=${offload} \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.actor.grad_clip=1.0 \
     actor_rollout_ref.actor.loss_agg_mode=${loss_agg_mode} \
     actor_rollout_ref.actor.ulysses_sequence_parallel_size=${sp_size} \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=${gen_tp} \
     actor_rollout_ref.rollout.enable_chunked_prefill=True \
     actor_rollout_ref.rollout.max_num_batched_tokens=$((max_prompt_length + max_response_length)) \
@@ -119,23 +122,23 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.ref.ulysses_sequence_parallel_size=${sp_size} \
     actor_rollout_ref.actor.fsdp_config.fsdp_size=${fsdp_size} \
     reward_model.reward_manager=dapo \
-    custom_reward_function.path=/root/projects/RLRAG/custom_reward.py \
+    custom_reward_function.path=/root/projects/RAGRL/src/custom_reward_v2.py \
     custom_reward_function.name=default_compute_score\
     +reward_model.reward_kwargs.overlong_buffer_cfg.enable=${enable_overlong_buffer} \
     +reward_model.reward_kwargs.overlong_buffer_cfg.len=${overlong_buffer_len} \
     +reward_model.reward_kwargs.overlong_buffer_cfg.penalty_factor=${overlong_penalty_factor} \
-    +reward_model.reward_kwargs.overlong_buffer_cfg.log=False \
+    +reward_model.reward_kwargs.overlong_buffer_cfg.log=True \
     +reward_model.reward_kwargs.max_resp_len=${max_response_length} \
-    trainer.logger=['console','wandb'] \
+    trainer.logger=['console','wandb','swanlab'] \
     trainer.project_name="${project_name}" \
     trainer.experiment_name="${exp_name}" \
-    trainer.n_gpus_per_node=4 \
+    trainer.n_gpus_per_node=${num_gpu} \
     trainer.nnodes=1 \
     trainer.val_before_train=False \
-    trainer.test_freq=20 \
-    trainer.save_freq=20 \
+    trainer.test_freq=50 \
+    trainer.save_freq=50 \
     trainer.total_epochs=10 \
     trainer.total_training_steps=400 \
     trainer.default_local_dir="${CKPTS_DIR}" \
     trainer.resume_mode=auto \
-    trainer.log_val_generations=5
+    trainer.log_val_generations=10 
