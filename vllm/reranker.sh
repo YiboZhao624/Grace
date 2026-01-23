7563d5fea26a80db50bf186c39f7bead661a2cd0
Yibo Zhao <90241849+YiboZhao624@users.noreply.github.com>
2026-01-23 13:42:54 +0800

    remove the absolute path

--- vllm/reranker.sh
+++ vllm/reranker.sh
@@ -2,6 +2,6 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
-vllm serve /root/shared_planing/LLM_model/BAAI-bge-reranker-v2-m3/\
+vllm serve /root/path/to/BAAI-bge-reranker-v2-m3/\
     --port 8002\
     --gpu_memory_utilization 0.2\
