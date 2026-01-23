325e918397a9d632efc55c6897d8273186182a93
Yibo Zhao <90241849+YiboZhao624@users.noreply.github.com>
2026-01-23 13:42:32 +0800

    remove the absolute path

--- vllm/llm.sh
+++ vllm/llm.sh
@@ -3,7 +3,7 @@
 export NCCL_P2P_DISABLE=1 
 export CUDA_VISIBLE_DEVICES=3,4
 python3 -m vllm.entrypoints.openai.api_server\
-    --model /root/shared_planing/LLM_model/Qwen2.5-7B-Instruct\
+    --model /root/path/to/Qwen2.5-7B-Instruct\
     --port 8003\
     --dtype bfloat16\
     --gpu-memory-utilization 0.9\
